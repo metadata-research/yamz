eb598acea0868c597ae6692b1e1c690608bcfc3b
Dillon Arevalo <dillonbarevalo@gmail.com>
2018-02-28 20:50:47 -0800

    Add logout for orcid before adding an orcid to an account

--- html/static/js/application.js
+++ html/static/js/application.js
@@ -1,9 +1,10 @@
 $(document).ready(function attachListeners () {
   expandCommentsListener();
+  logoutOrcidListener();
 });
 
-var expandCommentsListener = function() {
-  $('.expandComments').on('click', function(e){
+var expandCommentsListener = function () {
+  $('.expandComments').on('click', function (e) {
     e.preventDefault();
     var link = $(this);
     var divSuffix = link.data('id');
@@ -15,5 +16,23 @@ var expandCommentsListener = function() {
       link.html("Comments &#x25BC;");
       link.data('direction', 'down');
     }
+  })
+}
+
+var logoutOrcidListener = function () {
+  $('.orcid-login').on('click', function (e) {
+    var loginLink = $(this);
+    e.preventDefault();
+    $.ajax({
+        url: 'https://sandbox.orcid.org/userStatus.json?logUserOut=true',
+        dataType: 'jsonp',
+        success: function(result,status,xhr) {
+          window.location = loginLink.attr('href');
+        },
+        error: function (xhr, status, error) {
+          console.warn('failure')
+        }
+    });
+
   })
 }
