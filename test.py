58c877696efceb57f7bdfb3c3d9d0f786b4dc17b
Mark Phillips <mark.phillips@unt.edu>
2018-06-27 19:45:58 -0500

    flake8 passing for test.py

--- test.py
+++ test.py
@@ -1,63 +1,66 @@
 #!/usr/bin/python
-# Fix concept id's on terms.  
+# Fix concept id's on terms.
 
-import os, sys, optparse,re
-import json, psycopg2 as pqdb
+import sys
+import optparse
+import psycopg2 as pqdb
+
 import seaice
 
-## Parse command line options. ##
+#  Parse command line options. #
 
 parser = optparse.OptionParser()
 
-parser.add_option("--config", dest="config_file", metavar="FILE", 
-                  help="User credentials for local PostgreSQL database (defaults to '$HOME/.seaice'). " + 
-                       "If 'heroku' is given, then a connection to a foreign host specified by " + 
-                       "DATABASE_URL is established.",
+parser.add_option("--config", dest="config_file", metavar="FILE",
+                  help="User credentials for local PostgreSQL database" +
+                  "(defaults to '$HOME/.seaice'). " +
+                  "If 'heroku' is given, then a connection to a foreign host specified by " +
+                  "DATABASE_URL is established.",
                   default='heroku')
 
-parser.add_option("--role", dest="db_role", metavar="USER", 
+parser.add_option("--role", dest="db_role", metavar="USER",
                   help="Specify the database role to use for the DB connector pool. These roles " +
-                       "are specified in the configuration file (see --config).",
+                  "are specified in the configuration file (see --config).",
                   default="default")
 
 (options, args) = parser.parse_args()
 
 
-## Establish connection to PostgreSQL db ##
+#  Establish connection to PostgreSQL db #
 
 try:
 
-  if options.config_file == "heroku": 
-    
-    sea = seaice.SeaIceConnector()
+    if options.config_file == "heroku":
 
-  else: 
-    try: 
-      config = seaice.auth.get_config(options.config_file)
-    except OSError: 
-      print >>sys.stderr, "error: config file '%s' not found" % options.config_file
-      sys.exit(1)
+        sea = seaice.SeaIceConnector()
 
-    sea = seaice.SeaIceConnector(config.get(options.db_role, 'user'),       
-                                 config.get(options.db_role, 'password'),
-                                 config.get(options.db_role, 'dbname'))
-  
-  cur = sea.con.cursor()
-  cur.execute('SELECT NOW();') 
+    else:
+        try:
+            config = seaice.auth.get_config(options.config_file)
+        except OSError:
+            print >>sys.stderr, "error: config file '%s' not found" % options.config_file
+            sys.exit(1)
 
-  (t,) = cur.fetchone()
-  print '%s %s %s' % (t.day, seaice.pretty.monthOf[t.month - 1], t.year)
-    
-  ## Commit database mutations. ##
-  sea.commit()
+        sea = seaice.SeaIceConnector(config.get(options.db_role, 'user'),
+                                     config.get(options.db_role, 'password'),
+                                     config.get(options.db_role, 'dbname'))
 
-except pqdb.DatabaseError, e:
-  print 'error: %s' % e    
-  sys.exit(1)
+    cur = sea.con.cursor()
+    cur.execute('SELECT NOW();')
+
+    (t,) = cur.fetchone()
+    print('{} {} {}'.format(t.day, seaice.pretty.monthOf[t.month - 1], t.year))
+
+    #  Commit database mutations. #
+    sea.commit()
+
+except pqdb.DatabaseError as e:
+    print('error: {}'.format(e))
+    sys.exit(1)
 
 except IOError:
-  print >>sys.stderr, "error: file not found"
-  sys.exit(1)
+    print >>sys.stderr, "error: file not found"
+    sys.exit(1)
 
-except ValueError: 
-  print >>sys.stderr, "error: incorrect paramater type(s)"
+except ValueError:
+    print >>sys.stderr, "error: incorrect paramater type(s)"
