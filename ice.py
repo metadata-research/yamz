2f2a189e7e4307c4ec76bd7ecfad203948764d9b
Mark Phillips <mark.phillips@unt.edu>
2020-10-12 20:35:33 -0500

    p3 for ice.py

--- ice.py
+++ ice.py
@@ -24,10 +24,10 @@
 # ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-from __future__ import print_function
 import configparser
 from itertools import chain
-from urllib2 import Request, urlopen, URLError
+from urllib.request import Request, urlopen
+from urllib.error import URLError
 import sys
 import optparse
 import re
