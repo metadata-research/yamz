a1600261d131b8bf0bf47d93d84827dccba60c21
cr625 <cr625@drexel.edu>
2022-02-19 13:11:47 -0500

    update account form

--- app/user/forms.py
+++ app/user/forms.py
@@ -11,7 +11,7 @@ from wtforms.validators import DataRequired, Email, Length, EqualTo
 class EditProfileForm(FlaskForm):
     last_name = StringField("Last Name", validators=[DataRequired()])
     first_name = StringField("First Name", validators=[DataRequired()])
-    email = EmailField("Email", validators=[DataRequired()])
+    email = EmailField("Email", validators=[DataRequired(), Email()])
     orcid = StringField("ORCiD")
     receive_notifications = BooleanField("Receive email notifications")
     submit = SubmitField("Apply changes")
