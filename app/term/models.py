08a45f39abd1b90f13bd47e01a0bc4dd57201c6f
cr625 <cr625@drexel.edu>
2022-02-21 17:10:36 -0500

    update model

--- app/term/models.py
+++ app/term/models.py
@@ -1,12 +1,19 @@
 from app import db
 from sqlalchemy.dialects.postgresql import TSVECTOR
+import enum
+
+
+class si_class(enum.Enum):
+    vernacular = ("vernacular",)
+    canonical = ("canonical",)
+    depricated = ("depricated",)
 
 
 class Term(db.Model):
     __tablename__ = "terms"
     __table_args__ = {"schema": "si"}
-    id = db.Column(db.Integer, primary_key=True)
-    ark_id = db.Column(db.Integer, unique=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    ark_id = db.Column(db.Integer, unique=True, autoincrement=True)
     owner_id = db.Column(db.Integer, db.ForeignKey("si.users.id"))
     created = db.Column(db.DateTime, default=db.func.now())
     modified = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
@@ -18,9 +25,16 @@ class Term(db.Model):
     up = db.Column(db.Integer, default=0)
     down = db.Column(db.Integer, default=0)
     consensus = db.Column(db.Float, default=0)
-    # class = si.class
+    si_class = db.Column("value", si_class, default=si_class.vernacular)
     u_sum = db.Column(db.Integer, default=0)
     d_sum = db.Column(db.Integer, default=0)
     t_last = db.Column(db.DateTime)
     t_stable = db.Column(db.DateTime)
     tsv = db.Column(TSVECTOR)
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return "<Term %r>" % self.term_string
