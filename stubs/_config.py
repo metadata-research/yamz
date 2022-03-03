d4220ea426952fb14926d026968cd00c52ee0704
cr625 <cr625@drexel.edu>
2022-03-03 12:34:59 -0500

    add stub

---
+++ stubs/_config.py
@@ -0,0 +1,70 @@
+import os
+
+
+class Config(object):
+    # app
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "<your-secret-key>"
+
+    # database
+    SQLALCHEMY_DATABASE_URI = (
+        os.environ.get("SQL_ALCHEMY_DATABASE_URI")
+        or "postgresql://contributor:PASS@localhost/yamz"
+    )
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # oauth
+    OAUTH_CREDENTIALS = {
+        "google": {
+            "id": "<your-client-id>",
+            "secret": "<your-client-secret>",
+        },
+        "orcid": {
+            "id": "<your-client-id>",
+            "secret": "<your-client-secret>",
+        },
+    }
+
+    OAUTH_URLS = {
+        "orcid": {
+            "authorize_url": "https://orcid.org/oauth/authorize",
+            "access_token_url": "https://orcid.org/oauth/token",
+            "base_url": "https://orcid.org/",
+            "user_info_url": "https://pub.orcid.org/3.0/{}/person",
+        },
+        "google": {
+            "authorize_url": "https://accounts.google.com/o/oauth2/auth",
+            "access_token_url": "https://accounts.google.com/o/oauth2/token",
+            "base_url": "https://www.google.com/accounts/",
+            "user_info_url": "https://www.googleapis.com/oauth2/v2/userinfo",
+        },
+    }
+
+    # misc
+    SANDBOX = False
+    TERMS_PER_PAGE = 20
+
+    # ark realted settings
+    SHOULDER = "h"
+    ARK_PREFIX = "ark:/99152/"
+
+
+class TestConfig(Config):
+    TESTING = True
+    DEBUG = True
+
+    OAUTH_URLS = {
+        "orcid": {
+            "authorize_url": "https://sandbox.orcid.org/oauth/authorize",
+            "access_token_url": "https://sandbox.orcid.org/oauth/token",
+            "base_url": "https://sandbox.orcid.org/",
+            "user_info_url": "https://api.sandbox.orcid.org/v3.0/{}/person",
+        },
+        "google": {
+            "authorize_url": "https://accounts.google.com/o/oauth2/auth",
+            "access_token_url": "https://accounts.google.com/o/oauth2/token",
+            "base_url": "https://www.google.com/accounts/",
+            "user_info_url": "https://www.googleapis.com/oauth2/v2/userinfo",
+        },
+    }
+    SANDBOX = True
