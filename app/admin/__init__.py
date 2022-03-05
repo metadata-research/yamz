deeb7ffaacacedcdf366c38657817ed0b4f3c48c
cr625 <cr625@drexel.edu>
2022-03-05 01:15:29 -0500

    update admin interface

--- app/admin/__init__.py
+++ app/admin/__init__.py
@@ -2,4 +2,4 @@ from flask import Blueprint
 
 admin_blueprint = Blueprint("admin", __name__, template_folder="templates")
 
-from app.auth import views
+from app.admin import views
---
+++ app/admin/views.py
@@ -0,0 +1,9 @@
+from flask import render_template
+from flask_login import current_user, login_required
+
+from app.admin import admin_blueprint as admin
+
+
+@admin.route("/console")
+def index():
+    return render_template("admin/index.jinja")
