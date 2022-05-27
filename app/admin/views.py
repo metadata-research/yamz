8e41519274374bf92eaf3dcec3867ead63bd6aa8
cr625 <cr625@drexel.edu>
2022-05-27 09:53:01 -0400

    update admin view

--- app/admin/views.py
+++ app/admin/views.py
@@ -3,15 +3,8 @@ from flask_login import current_user, login_required
 from app.term.models import Term
 
 from app.admin import admin_blueprint as admin
-from app.admin.term import *
 
 
 @admin.route("/console")
 def index():
     return render_template("admin/index.jinja")
-
-
-@admin.route("/split")
-def split():
-    terms = findGCW()  # find all terms with the tag #{g: xqGCW | h1619}
-    return render_template("admin/split_terms.jinja", terms=terms)
