eb4f707d578a5ebb64d62b4998dd4042595130e6
cr625 <cr625@drexel.edu>
2022-08-04 22:04:33 -0400

    added file upload form

--- app/io/views.py
+++ app/io/views.py
@@ -1,12 +1,18 @@
-import io
-
 from flask import render_template
 
 
 from app.io import io_blueprint as io
 from flask import render_template
+from app.io.forms import FileUploadForm
+from app import documents
 
 
 @io.route("/import")
 def import_document():
-    return render_template("io/import_document.jinja")
+    form = FileUploadForm()
+    if form.validate_on_submit():
+        document_filename = documents.save(form.document_file.data)
+        document_url = documents.url(document_filename)
+        return "<a href='{}'>{}</a>".format(document_url, document_filename)
+
+    return render_template("io/import_document.jinja", form=form)
