9a554c45d511809a44ad48e2c1ea2b35212e001d
cr625 <cr625@drexel.edu>
2022-08-08 13:46:31 -0400

    update import script

--- app/io/data.py
+++ app/io/data.py
@@ -1,9 +1,41 @@
 import pandas
 
+from app.term.helpers import get_ark_id
+from app import db
+from flask import current_app
+from app.term.models import Term
+from flask_login import current_user
+
 # pandas is a little overkill but we'll use it more later
 
 
 def process_csv_upload(data_file):
     csv_dataframe = pandas.read_csv(data_file)
-    # import_file = pandas.read_csv("/home/cr625/yamz/uploads/data/test_1.csv")
     return csv_dataframe.to_dict(orient="records")
+
+
+def import_term_dict(term_dict):
+    for term in term_dict:
+        term_string = term["term"]
+        definition = term["definition"]
+        examples = term["examples"]
+
+        ark_id = get_ark_id()
+        shoulder = current_app.config["SHOULDER"]
+        naan = current_app.config["NAAN"]
+        ark = shoulder + str(ark_id)
+        owner_id = current_user.id
+
+        new_term = Term(
+            ark_id=ark_id,
+            shoulder=shoulder,
+            naan=naan,
+            owner_id=owner_id,
+            term_string=term_string,
+            definition=definition,
+            examples=examples,
+            concept_id=ark,
+        )
+        db.session.add(new_term)
+        db.session.commit()
+        db.session.refresh(new_term)
--- app/io/views.py
+++ app/io/views.py
@@ -3,7 +3,7 @@ from app.io import io_blueprint as io
 from app.io.forms import DataFileUploadForm
 from flask import render_template
 
-from app.io.data import process_csv_upload
+from app.io.data import import_term_dict, process_csv_upload
 
 
 @io.route("/upload", methods=["GET", "POST"])
@@ -11,7 +11,8 @@ def import_document():
     form = DataFileUploadForm()
     if form.validate_on_submit():
         uploaded_file = form.data_file.data
-        term_list = process_csv_upload(uploaded_file)
-        return render_template("io/import_results.jinja", term_list=term_list)
+        term_dict = process_csv_upload(uploaded_file)
+        import_success = import_term_dict(term_dict)
+        return render_template("io/import_results.jinja", term_list=term_dict)
 
     return render_template("io/import_document.jinja", form=form)
