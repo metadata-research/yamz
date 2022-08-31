fb6b81fb3f5ae32e4c7a7ba6c33669adb957caf7
cr625 <cr625@drexel.edu>
2022-08-31 12:43:56 -0400

    updated term relationships

--- app/term/models.py
+++ app/term/models.py
@@ -97,7 +97,7 @@ class Term(db.Model):
     concept_id = db.Column(db.String(64))
     status = db.Column("status", db.Enum(status), default=status.published)
     term_class = db.Column("class", db.Enum(term_class), default=term_class.vernacular)
-    term_set = db.Column(db.Integer, db.ForeignKey("termsets.id"))
+
     __ts_vector__ = db.Column(
         TSVECTOR,
         db.Computed(
@@ -112,11 +112,11 @@ class Term(db.Model):
 
     # relationships
 
-    termset = db.relationship("TermSet", primaryjoin="Term.term_set == TermSet.id")
-
     contributor = db.relationship("User", back_populates="terms")
 
     tags = db.relationship("Tag", secondary="term_tags", back_populates="terms")
+
+    termsets = db.relationship("TermSet", secondary="term_sets", back_populates="terms")
 
     tracks = db.relationship(
         "Track",
@@ -452,6 +452,13 @@ tag_table = db.Table(
     db.Column("term_id", db.Integer, db.ForeignKey("terms.id")),
 )
 
+set_table = db.Table(
+    "term_sets",
+    db.Model.metadata,
+    db.Column("set_id", db.Integer, db.ForeignKey("termsets.id")),
+    db.Column("term_id", db.Integer, db.ForeignKey("terms.id")),
+)
+
 
 db.event.listen(Term.definition, "set", Term.on_changed_definition)
 db.event.listen(Term.examples, "set", Term.on_changed_examples)
@@ -464,5 +471,9 @@ class TermSet(db.Model):
     description = db.Column(db.Text)
     created = db.Column(db.DateTime, default=db.func.now())
     updated = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
-
-    terms = db.relationship("Term", back_populates="termset")
+    terms = db.relationship(
+        "Term",
+        secondary="term_sets",
+        back_populates="termsets",
+        order_by="Term.term_string",
+    )
