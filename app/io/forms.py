13a9aaf8ea965246ca522d4d2a29c88eaff98801
cr625 <cr625@drexel.edu>
2022-08-31 15:40:09 -0400

    update upload form

--- app/io/forms.py
+++ app/io/forms.py
@@ -1,10 +1,12 @@
 from flask_wtf import FlaskForm
-from flask_wtf.file import FileField, FileRequired, FileAllowed
-
-from wtforms import SubmitField
+from flask_wtf.file import FileAllowed, FileField, FileRequired
+from wtforms import StringField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Length
 
 
 class DataFileUploadForm(FlaskForm):
+    name = StringField("Name Import", validators=[DataRequired()])
+    description = TextAreaField("Describe Import")
     data_file = FileField(
         "data_file",
         validators=[FileRequired(), FileAllowed(["csv"], "csv files only.")],
