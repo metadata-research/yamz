8b0a34e6bcc42f2aaa1e5b74503a9b5aac283dc0
cr625 <cr625@drexel.edu>
2023-01-07 18:40:24 +0000

    simplify cli

--- cli.py
+++ cli.py
@@ -16,108 +16,17 @@ db = SQLAlchemy(app)
 def cli():
     pass
 
-
-@click.command()
-def addusers():
-    add_users()
-
-
-@click.command()
-def addterms():
-    add_terms()
-
-
-@click.command()
-def addall():
-    add_users()
-    add_terms()
-
-
-@click.command()
-def transfertracking():
-    transfer_tracking()
-
-
-@click.command()
-def transfervotes():
-    transfer_votes
-
-
-@click.command()
-def transfertags():
-    transfer_tags()
-
-
 @click.command()
 @click.argument("email")
 def setsuperuser(email):
     set_superuser(email)
-
-
-@click.command()
-def taggcw():
-    tagGCW()
-
-
-@click.command()
-def printinner():
-    printInner()
-
-
-@click.command()
-def splitterms():
-    splitTerms()
-
-
-@click.command()
-def tagotherterms():
-    tagOtherTerms()
-
-
-@click.command()
-def removetagterms():
-    removeTagTerms_strings()
-
-
-@click.command()
-def cleantags():
-    clean_tags()
-
-
-@click.command()
-def convertambiguous():
-    convert_ambiguous()
-
 
 @click.command()
 def exportterms():
     export_terms()
 
 
-cli.add_command(addall)
-# first do:
-# flask db init
-# flask db migrate -m "initial migration"
-
-cli.add_command(addusers)  # 1
-cli.add_command(setsuperuser)  # 2
-cli.add_command(addterms)  # 3
-cli.add_command(transfertags)  # 4
-
-
-# we won't need this anymore
-cli.add_command(printinner)  # test the split terms function for GCW terms
-cli.add_command(splitterms)  # 5 actually split the terms
-
-cli.add_command(tagotherterms)
-
-# not used in initial migration
-cli.add_command(transfertracking)
-cli.add_command(transfervotes)
-cli.add_command(removetagterms)
-cli.add_command(cleantags)
-cli.add_command(convertambiguous)
-
+cli.add_command(setsuperuser)
 cli.add_command(exportterms)
 
 if __name__ == "__main__":
