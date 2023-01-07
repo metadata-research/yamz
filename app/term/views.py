b3959be98bdf4a4cdb84727a9063faca05a7013e
cr625 <cr625@drexel.edu>
2023-01-07 14:32:47 -0500

    update routes

--- app/term/views.py
+++ app/term/views.py
@@ -72,8 +72,8 @@ def format_date(date):
 def format_score(score):
     pass
 
-
-@term.route("ark:/99152/<concept_id>")
+@term.route("/ark:/99152/<concept_id>")
+@term.route("/ark:99152/<concept_id>")
 @term.route("/ark/<concept_id>")
 def display_term(concept_id):
     selected_term = Term.query.filter_by(concept_id=concept_id).first()
