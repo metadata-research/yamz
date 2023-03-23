00bf13df68c38a57c6924363cf5865248abcd0f0
John Kunze <105020+jkunze@users.noreply.github.com>
2023-03-22 19:37:13 -0700

    shrink search box to normal size on home page

--- app/main/views.py
+++ app/main/views.py
@@ -1,13 +1,12 @@
-from flask import render_template
+from flask import (render_template, g)
 from flask_login import current_user
 
 from app.main import main_blueprint as main
 from app.term.forms import SearchForm
 
-
 @main.route("/")
 def index():
-    search_form = SearchForm()
+    g.search_form = SearchForm()
     if current_user.is_authenticated:
         my_terms = current_user.terms
         tracked_terms = current_user.tracking
@@ -18,7 +17,7 @@ def index():
         "main/index.jinja",
         my_terms=my_terms,
         tracked_terms=tracked_terms,
-        search_form=search_form,
+        search_form=g.search_form,
     )
 
 
