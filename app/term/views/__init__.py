eb48d0d970774fafc0690d5cbb95ab1eff515a7d
Chris Rauch <cr625@drexel.edu>
2025-02-12 20:53:13 -0500

    Revert "Simplified"

--- app/term/views/__init__.py
+++
@@ -1,5 +0,0 @@
-from app.term.views.api_routes import *
-from app.term.views.tag_views import *
-from app.term.views.relationship_views import *
-from app.term.views.termset_views import *
-from app.term.views.term_views import *
